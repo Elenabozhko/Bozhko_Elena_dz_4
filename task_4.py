bbd49d673674ff39a1c066265bdd5ecb19436213
Elenabozhko <95896538+Elenabozhko@users.noreply.github.com>
2021-12-25 20:30:21 +0400

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,1 +1,13 @@
-pi
+# PS C:\Проекты\venv> pip show pillow
+# WARNING: Package(s) not found: pillow
+# PS C:\Проекты\venv> pip show pillow
+# WARNING: Package(s) not found: pillow
+
+
+# Installing collected packages: Pillow, pillow-svg
+# Successfully installed Pillow-8.4.0 pillow-svg-0.1.0
+# PS C:\Проекты\venv> pip freeze
+# Pillow==8.4.0
+# pillow-svg==0.1.0
+# PS C:\Проекты\venv>
+# # pip ins
